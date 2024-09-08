6b3112a2a66fa2d97cee2784cdf0f3c7bb235b07
Md Sujon Islam <iamsujonislam06@gmail.com>
2024-09-09 00:13:32 +0600

    add close icon

--- src/components/Intput.tsx
+++ src/components/Intput.tsx
@@ -1,8 +1,11 @@
+import { IoIosClose } from "react-icons/io";
+
 const Intput = () => {
   return (
     <div className="w-full h-full bg-purple-600 text-white/85 p-4 flex justify-center items-center rounded-lg">
-      <form action="#">
+      <form action="#" className="relative">
         <input className="p-2 outline-none rounded-md text-black" type="text" />
+        <IoIosClose className="text-black absolute top-2 right-28 text-3xl" />
         <button className="ml-2 px-4 py-2 border border-solid border-gray-400 rounded-md">
           Add Todo
         </button>
