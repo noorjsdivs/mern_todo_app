23ee9e730c784f85ff0be6feeec599a9f3c758bc
Md Sujon Islam <iamsujonislam06@gmail.com>
2024-09-10 08:58:58 +0600

    desing todoItem

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,3 +1,7 @@
+import { deleteTodo } from "@/app/redux/todoSlicer";
+import { MdDelete } from "react-icons/md";
+import { useDispatch } from "react-redux";
+
 interface Item {
   item: {
     todo: string;
@@ -6,10 +10,20 @@ interface Item {
 }
 
 const TodoItem = ({ item }: Item) => {
+  const dispatch = useDispatch();
   return (
-    <div>
-      <ul className="bg-green-950 text-white/90 p-3 my-2 rounded-lg hover:shadow-md hover:shadow-slate-300 duration-300 opacity-80 hover:opacity-100">
+    <div className="flex flex-col ">
+      <ul
+        className="bg-green-950 text-white/90 p-3 my-2 rounded-lg 
+      hover:shadow-sm hover:shadow-slate-400 duration-300 opacity-80 
+      hover:opacity-100 hover:skew-x-6 hover:translate-x-2 
+      flex justify-between items-center"
+      >
         <li>{item?.todo}</li>
+        <MdDelete
+          onClick={() => dispatch(deleteTodo(item?._id))}
+          className="text-lg hover:text-red-600 cursor-pointer"
+        />
       </ul>
     </div>
   );
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,17 +1,30 @@
 "use client";
 
-import { useSelector } from "react-redux";
+import { removeTodo } from "@/app/redux/todoSlicer";
+import { useDispatch, useSelector } from "react-redux";
 import { State } from "../../type";
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
+  const dispitch = useDispatch();
   const { todoList } = useSelector((state: State) => state?.todo);
 
   return (
     <div>
-      {todoList?.map((item) => (
-        <TodoItem key={item?._id} item={item} />
-      ))}
+      <div>
+        {todoList?.map((item) => (
+          <TodoItem key={item?._id} item={item} />
+        ))}
+      </div>
+      {todoList?.length > 0 && (
+        <button
+          onClick={() => dispitch(removeTodo())}
+          className="bg-blue-800 px-4 py-2 rounded-lg hover:shadow-sm hover:shadow-slate-400 duration-300 opacity-80 
+      hover:opacity-100 hover:skew-x-3 w-44 mx-auto mt-3"
+        >
+          Remove All Todo
+        </button>
+      )}
     </div>
   );
 };
