a8e0cfbd639c714b42c78da2312b5917c2267f73
Md Sujon Islam <iamsujonislam06@gmail.com>
2024-09-12 00:11:07 +0600

    alert box for remove all todo's button

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,6 +2,7 @@
 
 import { removeTodo } from "@/app/redux/todoSlicer";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { State } from "../../type";
 import TodoItem from "./TodoItem";
@@ -10,6 +11,7 @@ const TodoList = () => {
   const dispitch = useDispatch();
   const { todoList } = useSelector((state: State) => state?.todo);
   const pathName = usePathname();
+  const [removeAllTodo, setRemoveAllTodo] = useState(false);
 
   return (
     <div>
@@ -26,9 +28,36 @@ const TodoList = () => {
           ))}
         </div>
       </div>
+      {removeAllTodo && (
+        <div className="absolute inset-y-0 mt-10">
+          <div className=" bg-teal-700 rounded-lg shadow-lg drop-shadow-lg shadow-slate-400 p-4">
+            <h1 className="text-xl">Do you want to remove all Todo?</h1>
+            <div className="flex justify-around items-center my-3">
+              <button
+                onClick={() => {
+                  alert("Thank you for make sure!");
+                  setRemoveAllTodo(false);
+                }}
+                className="px-4 py-2 border-[1px] border-solid border-purple-500 rounded-lg"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={() => {
+                  dispitch(removeTodo());
+                  setRemoveAllTodo(false);
+                }}
+                className="px-4 py-2 border-[1px] border-solid border-purple-500 rounded-lg"
+              >
+                Yes
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
       {todoList?.length > 0 ? (
         <button
-          onClick={() => dispitch(removeTodo())}
+          onClick={() => setRemoveAllTodo(true)}
           className="bg-blue-800 text-white/90 px-4 py-2 rounded-lg hover:shadow-sm hover:shadow-slate-400 duration-300 opacity-80 
       hover:opacity-100 hover:skew-x-3 w-44 mx-auto mt-3"
         >
