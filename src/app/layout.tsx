67de6e9b3c7694cce5b12ee74df16e6f184024d5
AbdurRahim80 <mdrahim.nil2005@gmail.com>
2024-09-12 01:23:12 +0600

    add input fild and TodoList

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        classNameName={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
